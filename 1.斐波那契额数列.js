9777fb48c8f2645f75826a1c21d3a60a1f8f2170
canglan12138 <2575339383@qq.com>
2021-06-03 22:49:18 +0800

    Initial commit

---
+++ 1.斐波那契额数列.js
@@ -0,0 +1,25 @@
+function fun(n) {
+  // if (n <= 2) return 1
+  // return fun(n-1) + fun(n-2)
+  return n <= 2 ? 1 : fun(n-1) + fun(n-2)
+}
+
+console.log(fun(8));
+
+console.log('===============================');
+
+let arr = []
+arr[0] = 1
+arr[1] = 1
+
+function my_fun(n) {
+  for (let i = 2;i < n;i++) {
+    arr[i] = arr[i-1] + arr[i-2]
+  }
+
+  for (let i = 0;i < n;i++) {
+    console.log(i + ':' + arr[i]);
+  }
+}
+my_fun(8)
+
