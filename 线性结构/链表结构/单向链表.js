4f46a85f0554d6c13dcda89984a489871fd9b6f1
canglan12138 <2575339383@qq.com>
2021-06-07 20:48:39 +0800

    ''

--- 线性结构/链表结构/单向链表.js
+++ 线性结构/链表结构/单向链表.js
@@ -41,7 +41,7 @@ function LinkedList() {
   //3.insert
   LinkedList.prototype.insert = function (position,data) {
     //对 position 进行越界判断
-    if (position < 0 || position > this.length) return console.log(false)
+    if (position < 0 || position > this.length) return false
     //创建节点
     var new_node = new Node(data)
     var index = 0
@@ -63,26 +63,115 @@ function LinkedList() {
       this.length++
     }
   }
+
+  //4.RemoveAt 移除元素
+  LinkedList.prototype.RemoveAt = function (position) {
+    //判断是否有节点
+    if (this.length > 0) {
+      //position 进行越界判断
+      if (position < 0 || position > this.length) return false
+      let current_node = this.head
+      let pre = null
+      let index = 0
+      if (position === 0) { //删除第一个位置的节点
+        this.head = current_node.next
+      }else { //删除其余位置的节点
+        while (index < position) {
+          pre = current_node
+          current_node = current_node.next
+          index++
+        }
+        pre.next = current_node.next
+      }
+      this.length--
+      return current_node.data
+    }else {
+      return false
+    }
+  }
+
+  //5.indexOf 根据元素获取位置
+  LinkedList.prototype.indexOf = function (data) {
+    let current_node = this.head
+    let index = 0
+    if (current_node) {
+      while (current_node.data !== data) {
+        current_node = current_node.next
+        index++
+      }
+      return index
+    }else {
+      return false
+    }
+  }
+
+  //6.remove 根据元素删除信息
+  LinkedList.prototype.remove = function (data) {
+    let index = this.indexOf(data)
+    this.RemoveAt(index)
+  }
+
+  //7.isEmpty
+  LinkedList.prototype.isEmpty = function () {
+    return this.length === 0
+  }
+
+  //8.size
+  LinkedList.prototype.size = function () {
+    return this.length
+  }
+
+  //9.getFirst
+  LinkedList.prototype.getFirst = function () {
+    return this.head.data
+  }
 }
 
 var linkedlist = new LinkedList()
+//append
 linkedlist.append(1)
 linkedlist.append(2)
 linkedlist.append(3)
 linkedlist.append(4)
 linkedlist.append(5)
-console.log(linkedlist.head);
 
+//toString
+console.log('------toString----------');
 console.log(linkedlist.toString());
 
-linkedlist.insert(0,0)
+//insert
+console.log('--------insert--------');
+linkedlist.insert(0, 0)
 console.log(linkedlist.toString());
 
-linkedlist.insert(0,-1)
+linkedlist.insert(0, -1)
 console.log(linkedlist.toString());
 
-linkedlist.insert(3,'哈')
+linkedlist.insert(3, '哈')
 console.log(linkedlist.toString());
+
+//RemoveAt
+console.log('--------RemoveAt--------');
+console.log(linkedlist.RemoveAt(3));
+console.log(linkedlist.toString());
+console.log(linkedlist.length);
+
+//indexOf
+console.log('--------indexOf--------');
+console.log(linkedlist.indexOf(3));
+
+//remove
+console.log('--------remove--------');
+linkedlist.remove(5)
+console.log(linkedlist.toString());
+
+console.log('--------isEmpty--------');
+console.log(linkedlist.isEmpty());
+
+console.log('--------size--------');
+console.log(linkedlist.size());
+console.log('--------getFirst--------');
+console.log(linkedlist.getFirst());
 
 
 
